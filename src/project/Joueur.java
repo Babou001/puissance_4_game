6c7f99ce0cf333cff33440a08146f1b244c97e57
Heinkek-99 <44591037+Heinkek-99@users.noreply.github.com>
2024-10-14 14:42:30 +0200

    Add Jeton
    Ask

---
+++ src/project/Joueur.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package project;
+
+/**
+ * 
+ */
+public class Joueur {
+	public int numeroJoueur ;
+	Jeton jetonJoueur;
+	
+	public Joueur(){
+		
+	}
+	
+	public boolean placerJeton(int colonne, Grille grille) {
+		return (false);
+	}
+}
