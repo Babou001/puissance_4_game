7232802f81f914d28c1501248de16ae870e917cd
babouseye <el-hadji-babou.seye@etu.univ-amu.fr>
2024-10-14 15:02:39 +0200

    Grille and Cellule

---
+++ src/project/Cellule.java
@@ -0,0 +1,15 @@
+package src.project;
+
+import project.Jeton;
+
+public class Cellule {
+    private project.Jeton jeton;
+
+    public Cellule(Jeton jeton) {
+        this.jeton = jeton;
+    }
+
+    public boolean estVide(){
+
+    }
+}
--- src/project/Grille.java
+++ src/project/Grille.java
@@ -1,11 +1,24 @@
-/**
- * 
- */
-package project;
+package src.project;
 
-/**
- * 
- */
 public class Grille {
+    public int ligne = 6;
+    public int colonnes = 7;
+    private Cellule[][] plateau;
+
+    public void afficherGrille(){
+
+    }
+
+
+    public Cellule getCelluleByIndex(int ligne , int colonne){
+        return this.plateau[ligne][colonne] ;
+    }
+
+
+    public void setCelluleByIndex(int ligne , int colonne, project.Jeton jeton){
+        Cellule cellule = this.plateau[ligne][colonne];
+        cellule.set_jeton(jeton);
+
+    }
 
 }
